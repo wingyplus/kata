12edaae5af46a653df109ea5f6fc898698064ccd
Thanabodee <thanabodee@treebuild.com>
2014-05-28 11:58:05 +0700

    TEST PASS - add 2 players to tennis

--- tennis/tennis.go
+++ tennis/tennis.go
@@ -5,3 +5,19 @@ type Player struct{}
 func NewPlayer() *Player {
 	return &Player{}
 }
+
+type Tennis struct {
+	player1, player2 *Player
+}
+
+func NewTennis(player1, player2 *Player) *Tennis {
+	return &Tennis{player1, player2}
+}
+
+func (tenniz *Tennis) PlayerOne() *Player {
+	return tenniz.player1
+}
+
+func (tenniz *Tennis) PlayerTwo() *Player {
+	return tenniz.player2
+}
